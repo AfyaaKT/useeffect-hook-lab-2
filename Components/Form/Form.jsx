13cf4a88c337f78cbde5ca80e2f9dbf63918aa35
unknown <afyaakhudur@gmail.com>
2024-03-18 02:53:03 +0300

    complete

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,10 +1,14 @@
-export default function Form() {
+
+export default function Form({setInput , input}) {
   return (
     <form>
       <h3>Search user Github</h3>
       <div className="mb-3">
         <label>User name</label>
-        <input type="text" className="form-control" placeholder="User Name" />
+        <input type="text"
+         className="form-control"
+          placeholder="User Name"
+          onChange={(e)=>setInput(e.target.value)} />
       </div>
     </form>
   );
--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client"
+import { useEffect, useState } from 'react'
 import Form from '../Components/Form/Form'
 import UserCard from '../Components/UserCard/UserCard'
 import { Inter } from 'next/font/google'
@@ -6,10 +8,23 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+const [input,setInput]=useState('')
+const [data , setData]=useState({})
+
+async function  fetchData(){
+   const response = await fetch(`https://api.github.com/users/${input}`)
+  const result = await response.json();
+  setData(result)
+}
+
+useEffect(()=>{
+  fetchData()
+},[input])
+
   return (
     <div className="card">
-      <Form />
-      <UserCard />
+      <Form input = {input} setInput={setInput} />
+      <UserCard data={data} />
     </div>
   );
 }
